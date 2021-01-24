208238f4e653ccdb3f21dc0ab2b0e2f63b74ae5f
Jiten <jitenadav@gmail.com>
2021-01-24 22:35:15 +0530

    Added Config file

---
+++ config.py
@@ -0,0 +1,5 @@
+TESTING = True
+DEBUG = True
+FLASK_ENV = 'development'
+STATIC_FOLDER = 'static'
+TEMPLATES_FOLDER = 'templates'
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
