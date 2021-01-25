5ff3201e0e49824ad25c1fc9e6f0024d371abc6a
Jiten <jitenadav@gmail.com>
2021-01-25 11:34:05 +0530

    MySQL connected and Working

--- config.py
+++ config.py
@@ -3,3 +3,9 @@ DEBUG = True
 FLASK_ENV = 'development'
 STATIC_FOLDER = 'static'
 TEMPLATES_FOLDER = 'templates'
+
+#MYSQL CONFIGS
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root@123'
+MYSQL_DB = 'SEMDB'
--- server.py
+++ server.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+mysql = MySQL(app)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def home():
+    if request.method == "POST":
+        details = request.form
+        email = details['email']
+        password = details['password']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO demo(email, password) VALUES (%s, %s)", (email, password))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
     return render_template('index.html')
 
 if __name__ == '__main__':
