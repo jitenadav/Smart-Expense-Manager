75b7785e09710981c142a541e10d3f06c6a4340b
neeraj <neerajy341@gmail.com>
2021-01-27 14:37:48 +0530

    reset html added

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ SECRET_KEY = '\xb3\xe6\x18\xfe\xde\xfa\xf2h\xf3\x14\x90\x9b\xa8\x85\xe1\xeb\x81o
 #MYSQL CONFIGS
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'root@123'
+MYSQL_PASSWORD = 'Root@123'
 MYSQL_DB = 'SEMDB'
 
 #SQLAlchemy CONFIGS
-SQLALCHEMY_DATABASE_URI = 'mysql://root:root@123@localhost/SEMDB'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:Root@123@localhost/SEMDB'
