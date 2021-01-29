a50dc099ec6bc49a38a6bd29b48d3780d096cc30
Jiten <jitenadav@gmail.com>
2021-01-29 15:59:38 +0530

    Added Expense Table in models

--- models.py
+++ models.py
@@ -7,7 +7,9 @@ class User(db.Model):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     mobno = db.Column(db.Unicode(255), nullable=False)
+    regdate = db.Column(db.Datetime, nullable=False)
 
 
 class expenses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
